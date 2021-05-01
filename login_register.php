90ed280b2d3ae49704fbf0c5467778c9db5bc893
Tapasyagupte <49095501+Tapasyagupte@users.noreply.github.com>
2021-05-01 16:32:37 +0530

    Update login_register.php

--- login_register.php
+++ login_register.php
@@ -14,8 +14,12 @@
 		$upicture = mysqli_real_escape_string($conn, $upicture);
 
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
-		$insert_user = "INSERT INTO user_details (UserID, UserFullName, UserPassword, UserType, UserEmail, UserImage) VALUES ('$uid', '$uname', '$upass', '$utype', '$uemail', '$upicture')";
+		//$insert_user = "INSERT INTO user_details (UserID, UserFullName, UserPassword, UserType, UserEmail, UserImage) VALUES ('$uid', '$uname', '$upass', '$utype', '$uemail', '$upicture')";
 
+		$insert_user = "INSERT INTO user_details VALUES ('$uid', '$uname', '$upass', '$utype', '$uemail', '$upicture')";
+
+		
+		
 		//check password reconfirmation
 		if (($upass!=$upass1)){
 			$message="Password and re-enter password is incorrect. Please try again.";
