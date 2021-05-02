0099f0f92c4ea58320c96bdd2911a8ab346338b4
Tapasyagupte <49095501+Tapasyagupte@users.noreply.github.com>
2021-05-02 13:42:31 +0530

    Update connect.php

--- database/connect.php
+++ database/connect.php
@@ -4,10 +4,15 @@
 //$password = FALSE;
 //$dbname = "eproj";
 
+//$servername = "remotemysql.com";
+//$username = "BOadPXODJN";
+//$password = "yUDwIELXFv";
+//$dbname = "BOadPXODJN";
+
 $servername = "remotemysql.com";
-$username = "BOadPXODJN";
-$password = "yUDwIELXFv";
-$dbname = "BOadPXODJN";
+$username = "P1Z1pVnyDJ";
+$password = "Y7yrat7lHe";
+$dbname = "P1Z1pVnyDJ";
 
 //Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
